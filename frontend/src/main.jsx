8d75d41e8c1d4d815473e8b240eb11b3367e1566
Khokhlov Andrey <progeat@mail.ru>
2025-01-28 11:56:26 +0300

    Подключение css файла datepicker

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { App } from './App.jsx';
 import './index.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
